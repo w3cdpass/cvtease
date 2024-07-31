b310746bd8600caa69014903ad9481a3e8b24b4f
Kupasva <130740583+Kupasva@users.noreply.github.com>
2024-07-31 13:35:01 +0530

    -ed

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name='funny-face',
     version='0.1.0',
