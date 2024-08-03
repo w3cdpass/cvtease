af258d6f8d242febcb6de5e2392070b4c0e75826
Kupasva <130740583+Kupasva@users.noreply.github.com>
2024-08-03 10:27:05 +0530

    updated %code%

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import setup, find_packages
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='cvtease',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'click',
+        'colorama',
     ],
     entry_points={
         'console_scripts': [
-            'funny-face=funny_face.cli:main',
+            'cvtease=cvtease.cli:main',
         ],
     },
-    author='Kupasva',
+    author='w3cdpass',
     author_email='kupasva663@gmail.com',
     description='A CLI tool for computer vision that is under development. Includes a space shooter game using ASCII art.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/w3cdpass/cvtease',
     classifiers=[
@@ -25,5 +28,5 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
