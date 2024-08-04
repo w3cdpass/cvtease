8370d802063f7fce14346db5fc8999e7d988675c
Kupasva <130740583+Kupasva@users.noreply.github.com>
2024-08-04 11:26:35 +0530

    Github_readme.md

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-# Simple test case for the CLI
-def test_main():
-    from click.testing import CliRunner
-    from cvtease.cli import main
-
-    runner = CliRunner()
-    result = runner.invoke(main)
-    assert result.exit_code == 0
-    assert "This tool is under development, so be patient." in result.output
